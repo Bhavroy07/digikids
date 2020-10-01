c689c7a3b719f579918d8167ddf52bc6efbb2972
bhargav roy <bhargavbale80@gmail.com>
2020-10-01 21:07:56 +0530

    edit distance accuracy display

--- public/practice/assets/js/editdistance.js
+++ public/practice/assets/js/editdistance.js
@@ -5,12 +5,29 @@ function getvalues()
     var arr=new Array();
     for(var i=0;i<obj.length;i++)
     {
-        arr[i]=obj[i].textContent
+        var objj=document.getElementById(`ans${i+1}`)
+        arr[i]=objj.textContent
     }
     var arr2=["apple","dog","cat","elephant"];
+    var arr3=new Array();
     for(var i2=0;i2<arr.length;i2++)
     {
-    console.log(LevenshteinDistance(arr[i2],arr2[i2])+" ")
+    //console.log(LevenshteinDistance(arr[i2].toLowerCase(),arr2[i2]))
+    arr3.push((LevenshteinDistance(arr[i2].toLowerCase(),arr2[i2])));
+    }
+    for(var i3=0;i3<arr3.length;i3++)
+    {
+       var diff=arr2[i3].length-arr3[i3]
+       var accu=(diff/arr2[i3].length)*100
+       if(accu<=0)
+       {
+           var obj2=document.getElementById(`cans${i3+1}`)
+           obj2.textContent="You have 0% accuracy! Please try again."
+       }
+       else{
+        var obj2=document.getElementById(`cans${i3+1}`)
+        obj2.textContent=`You have ${accu}% accuracy!`
+       }
     }
     
     
@@ -18,26 +35,24 @@ function getvalues()
 
 }
 LevenshteinDistance =  function(a1, b1){
-    a=a1.toLowerCase()
-    b=b1.toLowerCase()
+    a=a1//.toLowerCase()
+    b=b1//.toLowerCase()
     if(a.length == 0) return b.length; 
     if(b.length == 0) return a.length; 
 
     var matrix = [];
 
-    // increment along the first column of each row
     var i;
     for(i = 0; i <= b.length; i++){
         matrix[i] = [i];
     }
 
-    // increment each column in the first row
+    
     var j;
     for(j = 0; j <= a.length; j++){
         matrix[0][j] = j;
     }
 
-    // Fill in the rest of the matrix
     for(i = 1; i <= b.length; i++){
         for(j = 1; j <= a.length; j++){
         if(b.charAt(i-1) == a.charAt(j-1)){
@@ -50,5 +65,5 @@ LevenshteinDistance =  function(a1, b1){
         }
     }
 
-return matrix[b.length][a.length];
+return matrix[b.length][a.length]-1;
 };
