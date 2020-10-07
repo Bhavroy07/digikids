fa2ea4678038c561eea73fb7f61e91090d9e2197
n4nimit2000 <n4nimit2000@gmail.com>
2020-10-07 16:26:10 +0530

    Merge

--- public/practice/assets/js/editdistance.js
+++ public/practice/assets/js/editdistance.js
@@ -10,16 +10,15 @@ function getvalues()
     var arr2=["apple","dog","cat","elephant"];
     for(var i2=0;i2<arr.length;i2++)
     {
-    console.log(LevenshteinDistance(arr[i2],arr2[i2])+" ")
+        console.log(LevenshteinDistance(arr[i2],arr2[i2])+" ")
     }
-    
-    
-    
-
 }
+
 LevenshteinDistance =  function(a1, b1){
     a=a1.toLowerCase()
     b=b1.toLowerCase()
+    a=a.trim();
+    b=b.trim();
     if(a.length == 0) return b.length; 
     if(b.length == 0) return a.length; 
 
@@ -27,28 +26,28 @@ LevenshteinDistance =  function(a1, b1){
 
     // increment along the first column of each row
     var i;
-    for(i = 0; i <= b.length; i++){
+    for(i = 0; i < b.length; i++){
         matrix[i] = [i];
     }
 
     // increment each column in the first row
     var j;
-    for(j = 0; j <= a.length; j++){
+    for(j = 0; j < a.length; j++){
         matrix[0][j] = j;
     }
 
     // Fill in the rest of the matrix
-    for(i = 1; i <= b.length; i++){
-        for(j = 1; j <= a.length; j++){
-        if(b.charAt(i-1) == a.charAt(j-1)){
-            matrix[i][j] = matrix[i-1][j-1];
-        } else {
+    for(i = 1; i < b.length; i++){
+        for(j = 1; j < a.length; j++){
+            if(b.charAt(i-1) == a.charAt(j-1)){
+                matrix[i][j] = matrix[i-1][j-1];
+            } else {
             matrix[i][j] = Math.min(matrix[i-1][j-1] + 1, // substitution
                                     Math.min(matrix[i][j-1] + 1, // insertion
                                             matrix[i-1][j] + 1)); // deletion
         }
-        }
     }
+}
 
-return matrix[b.length][a.length];
+return matrix[b.length-1][a.length-1];
 };
