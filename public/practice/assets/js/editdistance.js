377282fb7d8a59026343f2100f44aad86630a74e
n4nimit2000 <n4nimit2000@gmail.com>
2020-10-09 15:48:14 +0530

    Expert section quiz

--- public/practice/assets/js/editdistance.js
+++ public/practice/assets/js/editdistance.js
@@ -5,7 +5,7 @@ function getvalues()
     for(var i=0;i<obj.length;i++)
     {
         var objj=document.getElementById(`ans${i+1}`)
-        arr[i]=objj.textContent
+        arr[i]=objj.textContent;
     }
     var arr2=["apple","dog","cat","elephant","fish","giraffe"];
     var arr3=new Array();
@@ -13,32 +13,32 @@ function getvalues()
     {
     //console.log(LevenshteinDistance(arr[i2].toLowerCase(),arr2[i2]))
     arr3.push((LevenshteinDistance(arr[i2].toLowerCase(),arr2[i2])));
-    }
-    for(var i3=0;i3<arr3.length;i3++)
-    {
-       var diff=arr2[i3].length-arr3[i3]
-       var accu=(diff/arr2[i3].length)*100
-       if(accu<=0)
-       {
-           var obj2=document.getElementById(`cans${i3+1}`)
-           obj2.textContent="0%"
-       }
-       else{
-        var obj2=document.getElementById(`cans${i3+1}`)
-        obj2.textContent=`${accu}%`
-       }
-    }
+}
+for(var i3=0;i3<arr3.length;i3++)
+{
+ var diff=arr2[i3].length-arr3[i3]
+ var accu=(diff/arr2[i3].length)*100
+ if(accu<=0)
+ {
+     var obj2=document.getElementById(`cans${i3+1}`)
+     obj2.textContent="0%"
+ }
+ else{
+    var obj2=document.getElementById(`cans${i3+1}`)
+    obj2.textContent=`${accu}%`
+}
+}
 
-    var x = document.querySelectorAll(".hidden");
-    var y = document.querySelectorAll(".ques");
-    for (var i = 0; i < x.length; i++) 
-    {
-        x[i].style.display = "block";
-        y[i].style.height = "500px";
-    }
-    
-    
-    
+var x = document.querySelectorAll(".hidden");
+var y = document.querySelectorAll(".ques");
+for (var i = 0; i < x.length; i++) 
+{
+    x[i].style.display = "block";
+    y[i].style.height = "500px";
+}
+
+
+
 
 }
 LevenshteinDistance =  function(a1, b1){
@@ -62,15 +62,15 @@ LevenshteinDistance =  function(a1, b1){
 
     for(i = 1; i <= b.length; i++){
         for(j = 1; j <= a.length; j++){
-        if(b.charAt(i-1) == a.charAt(j-1)){
-            matrix[i][j] = matrix[i-1][j-1];
-        } else {
+            if(b.charAt(i-1) == a.charAt(j-1)){
+                matrix[i][j] = matrix[i-1][j-1];
+            } else {
             matrix[i][j] = Math.min(matrix[i-1][j-1] + 1, // substitution
                                     Math.min(matrix[i][j-1] + 1, // insertion
                                             matrix[i-1][j] + 1)); // deletion
         }
-        }
     }
+}
 
 return matrix[b.length][a.length]-1;
 };
