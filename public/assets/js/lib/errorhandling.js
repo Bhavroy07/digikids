a00e10670bfd9a2417f14efeb901d63b85295315
n4nimit2000 <n4nimit2000@gmail.com>
2020-10-22 20:33:51 +0530

    Error handling code for frontend

--- public/assets/js/lib/errorhandling.js
+++ public/assets/js/lib/errorhandling.js
@@ -1,8 +1,10 @@
+var arr = new Array();
+arr=["Invalid credentials! Please try again.","Invalid OTP","OTP expired","Passwords do not match! Try again"];
+var ids=["msg-auth","msg-otp","msg-otp","msg-pass"];
 function errorhandle(x){
-    var arr = new Array()
-    arr=["Invalid credentials! Please try again.","Invalid OTP","OTP expired","Passwords do not match! Try again"]
-    console.log(x)
-    
+	//console.log(x);
+	var msg="<strong>"+arr[x]+"</strong>";
+	document.getElementById(ids[x]).innerHTML=msg;
 }
 
-module.exports=errorhandle
+module.exports=errorhandle;
